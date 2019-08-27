9dd5b257e66585c1293fe643c5c18468f60f1dce
mn <1490949269@qq.com>
2019-08-27 11:03:43 +0800

    重构

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # refactoring_step_by_step
+
+1. 写Test类
+2. Diver类中：prublic int age; 改为 private int age;
+3. Police类中：driver.age >= 18改为driver.getAge() >= 18
--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -1,7 +1,15 @@
 package com.tws.refactoring;
 
 public class Driver {
-    public int age;
+    public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	private int age;
 
     public Driver(int age) {
         this.age = age;
